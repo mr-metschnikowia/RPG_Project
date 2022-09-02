7aca0d43538832acf82926cebe64a9bb883e3a0c
mr-metschnikowia <gorbunov.lev@googlemail.com>
2022-09-02 13:26:53 +0100

    jump animation integration

--- Assets/Scripts/characterAnimator.cs
+++ Assets/Scripts/characterAnimator.cs
@@ -6,30 +6,38 @@ public class characterAnimator : MonoBehaviour
 {
     Animator animator;
 
+    CharacterController controller;
+
     float moving = 0f;
 
     Vector3 oldPosition; 
 
     void Start()
     {
+        controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
         oldPosition = transform.position;
     }
 
     void checkIfMoving() {
-        if (transform.position != oldPosition)
-        {
+        if (!controller.isGrounded) {
             moving = 1;
         }
-        else {
+        // jump block 
+        else if (transform.position != oldPosition)
+        {
+            moving = 0.5f;
+        } else {
             moving = 0;
         }
         oldPosition = transform.position;
     }
+    // set animation trigger based on player movement 
 
     void Update()
     {
         checkIfMoving();
         animator.SetFloat("speed", moving, .1f, Time.deltaTime);
     }
+    // send animation trigger to animator 
 }
